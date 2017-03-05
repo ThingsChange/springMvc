b9c125b7469f4655a1039ca76437aba73ebcc2b1
Andrew <amoteyatou1314>
2017-03-06 00:05:49 +0800

    修改文件名

--- src/main/webapp/static/gulpfile.js
+++ src/main/webapp/static/gulpfile.js
@@ -1,91 +1,124 @@
-//导入你所需要用的工具包 require('node_modules里对应模块')
+//引入gulp和gulp插件
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'), // sass/scss编译
-    babel=require('gulp-babel'),
-    rename=require('gulp-rename'),
-    jshint = require("gulp-jshint"),
-    uglify=require('gulp-uglify'),
+    // less = require('gulp-less'),
+    assetRev = require('gulp-asset-rev'),
+    minifyCss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    htmlmin = require('gulp-htmlmin'),
+    rename = require('gulp-rename'),
+    // imagemin = require('gulp-imagemin'),
     runSequence = require('run-sequence'),
     rev = require('gulp-rev'),
     revCollector = require('gulp-rev-collector');
 
-var es6Src='source/**/*.es6',
-    jsSrc='dist/**/*.js',
-    cssSrc='css/**/*.css';
+//定义css、js源文件路径
+var cssSrc = 'css/**/*.css',
+    cssMinSrc = 'dist/css/*.css',
+    jsSrc = 'source/**/*.js',
+    jsMinSrc = 'dist/js/**/*.js',
+    lessSrc = 'less/**/*.less',
+    imgMinSrc = 'dist/images/*.{png,jpg,gif,ico}',
+    htmlSrc = '../*.jsp';
 
+/*
+//编译less 定义一个less任务（自定义任务名称）
+gulp.task('less', function(){
+    return gulp.src(lessSrc)  //该任务针对的文件
+        .pipe(less()) //该任务调用的模块
+        .pipe(gulp.dest('css'));//编译后的路径
+});
+*/
 
-//转换es6文件为es5代码
-gulp.task("toes5", function () {
-    return gulp.src(es6Src)// ES6 源码存放的路径
-        .pipe(babel())
-        .pipe(gulp.dest("dist")); //转换成 ES5 存放的路径
+//为css中引入的图片/字体等添加hash编码
+gulp.task('assetRev', function(){
+    return gulp.src(cssSrc)  //该任务针对的文件
+        .pipe(assetRev()) //该任务调用的模块
+        .pipe(gulp.dest('src')); //编译后的路径
 });
-// 压缩 js 文件
-gulp.task('min', function() {
-    gulp.src(jsSrc)
-        .pipe(uglify({
-            mangle: true,  //类型：Boolean 默认：true 是否修改变量名
-            compress: true,//类型：Boolean 默认：true 是否完全压缩
-            preserveComments:'some'//并保留部分注释
-        }))
-        .pipe(rename({ suffix: '.min' })) // 重命名
-        .pipe(gulp.dest('js'))
-});
-gulp.task('auto', function () {
-    // 监听文件修改，当文件被修改则执行 script 任务
-    gulp.watch(es6Src, ['toes5']);
-    gulp.watch(jsSrc, ['min']);
-    gulp.watch('css/**/*.scss',['sass']);
-    gulp.watch(jsSrc,['revCss','revJs','revHtml'])
-});
-// scss 任务
-gulp.task('sass', function () {
-    return sass('css/**/*.scss', { style: 'compressed' }) // 指明源文件路径、并进行文件匹配（style: 'compressed' 表示输出格式）
-        .on('error', function (err) {
-            console.error('Error!', err.message); // 显示错误信息
-        })
-        .pipe(gulp.dest('minicss')); // 输出路径
-});
-// 默认任务
-gulp.task('default',['auto','sass','watch1','dev']);
-//监听文件
-gulp.task('watch1',function(){
-    return gulp.watch('src/css/test.scss',['sass']);
-    //监听 src/css/test.scss 文件，修改时自动执行 sass 任务。
+
+//压缩css
+gulp.task('cssMin', function() {
+    return gulp.src(cssSrc)   //压缩的文件
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifyCss()) //执行压缩
+        .pipe(gulp.dest('dist/css'));  //输出文件夹
 });
 
 //CSS生成文件hash编码并生成 rev-manifest.json文件名对照映射
 gulp.task('revCss', function(){
-    return gulp.src(cssSrc)
-        .pipe(rev())
-        .pipe(rev.manifest())
-        .pipe(gulp.dest('rev/css'));
+    return gulp.src(cssMinSrc)
+        .pipe(rev()) //文件名加MD5后缀
+        .pipe(rev.manifest())  //必须有这个方法 生成一个rev-manifest.json
+        .pipe(gulp.dest('dist/css'));  //将rev-manifest.json 保存到 dist/css 目录内
 });
 
+//压缩js
+gulp.task('uglify',function(){
+    return gulp.src(jsSrc)
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
 
 //js生成文件hash编码并生成 rev-manifest.json文件名对照映射
 gulp.task('revJs', function(){
-    return gulp.src(jsSrc)
+    return gulp.src(jsMinSrc)
         .pipe(rev())
         .pipe(rev.manifest())
-        .pipe(gulp.dest('rev/js'));
+        .pipe(gulp.dest('dist/js'));
 });
 
 
-//Html替换css、js文件版本
-gulp.task('revHtml', function () {
-    return gulp.src(['rev/**/*.json', '../*.jsp'])
-        .pipe(revCollector())
+//压缩html
+gulp.task('htmlMin',function(){
+    var options = {
+        collapseWhitespace:true,  //从字面意思应该可以看出来，清除空格，压缩html，这一条比较重要，作用比较大，引起的改变压缩量也特别大。
+        collapseBooleanAttributes:true,  //省略布尔属性的值，比如：<input checked="checked"/>,那么设置这个属性后，就会变成 <input checked/>。
+        removeComments:true,  //清除html中注释的部分，我们应该减少html页面中的注释。
+        removeEmptyAttributes:true,  //清除所有的空属性。
+        removeScriptTypeAttributes:true,  //清除所有script标签中的type="text/javascript"属性。
+        removeStyleLinkTypeAttributes:true,  //清楚所有Link标签上的type属性。
+        minifyJS:true,  //压缩html中的javascript代码。
+        minifyCSS:true  //压缩html中的css代码。
+    };
+    return gulp.src(htmlSrc)
+        .pipe(htmlmin(options))
         .pipe(gulp.dest('../'));
 });
 
+//Html替换css、js文件版本
+gulp.task('revHtml', function () {
+    return gulp.src(['dist/**/*.json', '../*.jsp'])
+        .pipe(revCollector())
+        .pipe(gulp.dest('../'));
+});
+/*
+//压缩image
+gulp.task('imageMin', function () {
+    gulp.src('images/!*.{png,jpg,gif,ico}')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
+});*/
 
-//开发构建
-gulp.task('dev', function (done) {
-    condition = false;
-    runSequence(
+gulp.task('revImage', function(){
+    return gulp.src(imgMinSrc)
+        .pipe(rev())
+        .pipe(rev.manifest())  //必须有这个方法
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('default', function (done) {
+    //condition = false;
+    runSequence(  //此处不能用gulp.run这个最大限度并行(异步)执行的方法，要用到runSequence这个串行方法(顺序执行)才可以在运行gulp后顺序执行这些任务并在html中加入版本号
+        // 'less',
+        ['assetRev'],
+        ['cssMin'],
         ['revCss'],
+        ['uglify'],
         ['revJs'],
+        // 'imageMin',
+        ['revImage'],
+        ['htmlMin'],
         ['revHtml'],
         done);
 });
